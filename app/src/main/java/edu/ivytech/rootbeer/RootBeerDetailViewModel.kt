8012cb1bf0b00ba43ffe166d3777b7d460af22a8
Angela Venable <avenable11@ivytech.edu>
2026-02-06 13:18:59 -0500

    update to build in 2026. appears to still work but has many warnings.

--- app/src/main/java/edu/ivytech/rootbeer/RootBeerDetailViewModel.kt
+++ app/src/main/java/edu/ivytech/rootbeer/RootBeerDetailViewModel.kt
@@ -2,15 +2,16 @@ package edu.ivytech.rootbeer
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
+//import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.switchMap
 import edu.ivytech.rootbeer.database.RootBeer
 import java.util.*
 
 class RootBeerDetailViewModel : ViewModel() {
     private val repo = RootBeerRepository.get()
     private val rootBeerIdLiveData = MutableLiveData<UUID>()
-    var rootBeerLiveData : LiveData<RootBeer> = Transformations.switchMap(rootBeerIdLiveData) {
+    var rootBeerLiveData : LiveData<RootBeer> = rootBeerIdLiveData.switchMap() {
             id -> repo.getRootBeer(id)
     }
     fun loadRootBeer(id : UUID) {
